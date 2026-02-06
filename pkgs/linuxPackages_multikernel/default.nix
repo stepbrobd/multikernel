b5a15c360116c10ccae8d52c7894d012a50d51c0
Yifei Sun <ysun@hey.com>
2026-02-06 18:51:42 +0100

    no need to with

--- pkgs/linuxPackages_multikernel/default.nix
+++ pkgs/linuxPackages_multikernel/default.nix
@@ -36,39 +36,37 @@ linuxPackagesFor (
   ) { }
 )
 // {
-  test =
-    with inputs.self.nixosModules;
-    testers.runNixOSTest {
-      name = "multikernel";
+  test = testers.runNixOSTest {
+    name = "multikernel";
 
-      interactive.sshBackdoor.enable = true;
+    interactive.sshBackdoor.enable = true;
 
-      nodes.machine = {
-        imports = [ multikernel ];
+    nodes.machine = {
+      imports = [ inputs.self.nixosModules.multikernel ];
 
-        # reserved for multikernel, check with /proc/iomem
-        boot.kernelParams = [
-          "mem=1G"
-          "mkkernel_pool=1G@1G"
-        ];
+      # reserved for multikernel, check with /proc/iomem
+      boot.kernelParams = [
+        "mem=1G"
+        "mkkernel_pool=1G@1G"
+      ];
 
-        boot.kernelModules = [ "edd" ];
+      boot.kernelModules = [ "edd" ];
 
-        virtualisation = {
-          cores = 2;
-          memorySize = 2048;
-        };
+      virtualisation = {
+        cores = 2;
+        memorySize = 2048;
       };
-
-      testScript = ''
-        machine.succeed("zcat /proc/config.gz | grep CONFIG_MULTIKERNEL=y")
-        machine.succeed("mount -t multikernel none /sys/fs/multikernel")
-        machine.succeed("ls -d /sys/fs/multikernel/")
-        machine.succeed("grep 'Multikernel Memory Pool' /proc/iomem")
-        machine.succeed("kerf init --cpus 1")
-        machine.succeed("kerf create test --cpus 1 --memory 1GB")
-        # loading and unloading currently does not work
-        # machine.succeed("kerf load test --kernel $(realpath /run/current-system/kernel) --initrd $(realpath /run/current-system/initrd)")
-      '';
     };
+
+    testScript = ''
+      machine.succeed("zcat /proc/config.gz | grep CONFIG_MULTIKERNEL=y")
+      machine.succeed("mount -t multikernel none /sys/fs/multikernel")
+      machine.succeed("ls -d /sys/fs/multikernel/")
+      machine.succeed("grep 'Multikernel Memory Pool' /proc/iomem")
+      machine.succeed("kerf init --cpus 1")
+      machine.succeed("kerf create test --cpus 1 --memory 1GB")
+      # loading and unloading currently does not work
+      # machine.succeed("kerf load test --kernel $(realpath /run/current-system/kernel) --initrd $(realpath /run/current-system/initrd)")
+    '';
+  };
 }
