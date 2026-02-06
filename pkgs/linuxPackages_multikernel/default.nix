637a1a3ab84f967a79f10af70767548d6679bfd1
Yifei Sun <ysun@hey.com>
2026-02-06 17:22:19 +0100

    first part already work?!

--- pkgs/linuxPackages_multikernel/default.nix
+++ pkgs/linuxPackages_multikernel/default.nix
@@ -43,10 +43,32 @@ linuxPackagesFor (
 
       interactive.sshBackdoor.enable = true;
 
-      nodes.machine.imports = [ multikernel ];
+      nodes.machine = {
+        imports = [ multikernel ];
+
+        # reserved for multikernel, check with /proc/iomem
+        boot.kernelParams = [
+          "mem=1G"
+          "mkkernel_pool=1G@1G"
+        ];
+
+        boot.kernelModules = [ "edd" ];
+
+        virtualisation = {
+          cores = 2;
+          memorySize = 2048;
+        };
+      };
 
       testScript = ''
         machine.succeed("zcat /proc/config.gz | grep CONFIG_MULTIKERNEL=y")
+        machine.succeed("mount -t multikernel none /sys/fs/multikernel")
+        machine.succeed("ls -d /sys/fs/multikernel/")
+        machine.succeed("grep 'Multikernel Memory Pool' /proc/iomem")
+        machine.succeed("kerf init --cpus 1")
+        machine.succeed("kerf create test --cpus 1 --memory 1GB")
+        # loading and unloading currently does not work
+        # machine.succeed("kerf load test --kernel $(realpath /run/current-system/kernel) --initrd $(realpath /run/current-system/initrd)")
       '';
     };
 }
